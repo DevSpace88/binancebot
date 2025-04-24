6420fc591e9affbe13e55adf13282b98f44bb7e1
sergejgorochow <s.gorochow@netstack.de>
2025-04-24 11:59:59 +0200

    removed chart, since it was just causing problems

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,30 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   lintOnSave: false,  // ESLint beim Speichern deaktivieren
+//   devServer: {
+//     port: 8080,
+//     proxy: {
+//       '/api': {
+//         target: 'http://tradebot-api:8000',  // Bei Docker-Compose auf den Service-Namen verweisen
+//         changeOrigin: true,
+//         pathRewrite: {
+//           '^/api': '/api'
+//         },
+//         logLevel: 'debug'
+//       }
+//     }
+//   },
+//   // Feature-Flags für Vue 3 explizit definieren (behebt die Warnung)
+//   chainWebpack: config => {
+//     config.plugin('define').tap(args => {
+//       const featureFlags = args[0].__VUE_OPTIONS_API__ ? args[0] : args[0]['process.env'];
+//       featureFlags.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
+//       return args;
+//     });
+//   }
+// })
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -9,17 +36,19 @@ module.exports = defineConfig({
         target: 'http://tradebot-api:8000',  // Bei Docker-Compose auf den Service-Namen verweisen
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '/api'
+          '^/api': '/api'  // Keine Änderung am Pfad
         },
         logLevel: 'debug'
       }
     }
   },
-  // Feature-Flags für Vue 3 explizit definieren (behebt die Warnung)
+  // Feature-Flags für Vue 3 explizit definieren
   chainWebpack: config => {
     config.plugin('define').tap(args => {
-      const featureFlags = args[0].__VUE_OPTIONS_API__ ? args[0] : args[0]['process.env'];
-      featureFlags.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
+      const existing = args[0]['process.env'] || args[0];
+      existing.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = 'false';
+      existing.__VUE_PROD_DEVTOOLS_GLOBAL_HOOK__ = 'false';
+      existing.__VUE_OPTIONS_API__ = 'true';
       return args;
     });
   }
